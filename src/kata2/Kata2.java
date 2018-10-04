e1ae31e7b09905c28b0283d192e988d013c84721
Usuario <Usuario@10.22.148.99>
2018-10-04 11:16:00 +0100

    Segunda versión de Kata2, esta vez utilizando un operador ternario, al contrario que en la primera que utilizabamos los if

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -9,15 +9,28 @@ public class Kata2 {
     public static void main(String[] args) {
         int[] array = {1,3,2,6,8,4,2,2,5,6,9,1,4,4,4,6,1,2,9,3,2,0,0,1};
         Map<Integer, Integer> histogram  = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            if (histogram.containsKey(array[i])){
-                histogram.put(array[i],histogram.get(array[i])+ 1);
-            }else{
-                histogram.put(array[i],1);
-            }
+        for (int i : array) {
+            histogram.put(i,histogram.containsKey(i) ? 
+                    histogram.get(i)+ 1 : 1);
+        } /*
+        
+0==>2
+1==>4
+2==>5
+3==>2
+4==>4
+5==>1
+6==>3
+8==>1
+9==>2
+        */
+    
+            
+            
+            
         
             
-        }
+        
         for (Integer key : histogram.keySet()) {
             System.out.println(key + "==>" + histogram.get(key));
         }
