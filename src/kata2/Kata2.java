94d01f91d78eeb7161da3ee6a43ae912c0f74440
Usuario <Usuario@10.22.148.99>
2018-10-04 12:00:52 +0100

    Quinta versión de Kata2 en la que el array ha pasado de contener enteros a strings, para así comprobar el funcionamiento de la clase de tipo genérico.

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,29 +7,17 @@ public class Kata2 {
 
 
     public static void main(String[] args) {
-        Integer[] array = {1,3,2,6,8,4,2,2,5,6,9,1,4,4,4,6,1,2,9,3,2,0,0,1};
+        String[] array = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         Histogram histo = new Histogram(array); 
-        Map<Integer,Integer> histogr = histo.getHistogram();
-         /*
-        
-0==>2
-1==>4
-2==>5
-3==>2
-4==>4
-5==>1
-6==>3
-8==>1
-9==>2
-        */
-    
+        Map<String,Integer> histogr = histo.getHistogram();
+         
             
             
             
         
             
         
-        for (Integer key : histogr.keySet()) {
+        for (String key : histogr.keySet()) {
             System.out.println(key + "==>" + histogr.get(key));
         }
             
